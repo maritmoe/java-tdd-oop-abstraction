9786e532e1e7e750950e48a50cc324f98f4726d3
Marit <marit.moe2@ntebb.no>
2024-01-18 11:09:55 +0100

    Exercise 1 morning activity

--- src/main/java/com/booleanuk/core/TodoItem.java
+++ src/main/java/com/booleanuk/core/TodoItem.java
@@ -1,21 +1,45 @@
 package com.booleanuk.core;
 
+
+// Morning activity
 public class TodoItem {
-    public String title;
-    public String detail;
-    public String status;
+    private String title;
+    private String detail;
+    private String status;
 
     public TodoItem(String title, String detail, String status) {
+        this.setTitle(title);
+        this.setDetail(detail);
+        this.setStatus(status);
+    }
+
+    public void setTitle(String title) {
+
         this.title = title;
+    }
+
+    public void setDetail(String detail) {
+
         this.detail = detail;
-        this.status = status;
     }
 
     public void setStatus(String status) {
+
         this.status = status;
     }
 
+    public String getTitle() {
+
+        return this.title;
+    }
+
+    public String getDetail() {
+
+        return this.detail;
+    }
+
     public String getStatus() {
+
         return this.status;
     }
 }
--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -3,6 +3,40 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+// Morning activity
 class TodoItemTest {
 
+    @Test
+    public void testGetTitle() {
+        TodoItem t = new TodoItem("Run", "Long", "Done");
+        Assertions.assertEquals("Run", t.getTitle());
+    }
+    @Test
+    public void testGetDetail() {
+        TodoItem t = new TodoItem("Run", "Long", "Done");
+        Assertions.assertEquals("Long", t.getDetail());
+    }
+    @Test
+    public void testGetStatus() {
+        TodoItem t = new TodoItem("Run", "Long", "Done");
+        Assertions.assertEquals("Done", t.getStatus());
+    }
+    @Test
+    public void testSetTitle() {
+        TodoItem t = new TodoItem("Run", "Long", "Done");
+        t.setTitle("Walk");
+        Assertions.assertEquals("Walk", t.getTitle());
+    }
+    @Test
+    public void testSetDetail() {
+        TodoItem t = new TodoItem("Run", "Long", "Done");
+        t.setDetail("Short");
+        Assertions.assertEquals("Short", t.getDetail());
+    }
+    @Test
+    public void testSetStatus() {
+        TodoItem t = new TodoItem("Run", "Long", "Done");
+        t.setStatus("Undone");
+        Assertions.assertEquals("Undone", t.getStatus());
+    }
 }
